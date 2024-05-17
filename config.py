19130beb36125ea00b67fbe099e10e13fb6b4870
Mauricio Pizarro <pizarromauricio031096@gmail.com>
2024-05-17 01:11:49 -0300

    delete-rabbit-env-configs (#5)

--- config.py
+++ config.py
@@ -3,10 +3,6 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_MONGO_URL: str
-    RABBIT_USERNAME: str
-    RABBIT_PASSWORD: str
-    RABBIT_HOST: str
-    RABBIT_VHOST: str
 
     class Config:
         env_file = './.env'
